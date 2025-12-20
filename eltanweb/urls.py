52553bb675696953f3935142590f447cc72e82b7
Jonah Onah <onahjonah@gmail.com>
2025-12-20 05:41:50 +0100

    temporal static files

--- eltanweb/urls.py
+++ eltanweb/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     #path('summernote/', include('django_summernote.urls')),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
